9d25763b9b7508d9b8e5384a5ed08e0bffcdb595
aniketrb-github <bharsakaleaniket@gmail.com>
2021-09-23 17:51:29 +0530

    initial commit - spock unit test

---
+++ spring-spock/src/main/java/com/arbtech/springspock/SpringSpockApplication.java
@@ -0,0 +1,13 @@
+package com.arbtech.springspock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpockApplication.class, args);
+	}
+
+}
---
+++ spring-spock/src/test/java/com/arbtech/springspock/SpringSpockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbtech.springspock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
